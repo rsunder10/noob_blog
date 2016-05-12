13018bda209893b788f321941501d8b245527495
sunder <sunderis10@gmail.com>
2016-05-12 23:35:07 +0530

    CRUD completion

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,10 @@
 class ArticlesController < ApplicationController
 
+
+  def index
+    @article=Article.all
+  end
+
   def new
     @article =Article.new
 
@@ -20,6 +25,30 @@ class ArticlesController < ApplicationController
   def show
     @article =Article.find(params[:id]) 
 
+  end
+
+  def edit
+
+    @article=Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice]="Article was sucessfully Updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+
+
+  end
+
+  def destroy
+    @article=Article.find(params[:id])
+    @article.destroy
+    flash[:notice]="Article was Deleted"
+    redirect_to articles_path
   end
 
   private
