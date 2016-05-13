332003097e9aa5fa5f8d36bb49c17d46f4433022
sunder <sunderis10@gmail.com>
2016-05-13 08:16:12 +0530

    Redundancy removed

--- Gemfile
+++ Gemfile
@@ -43,5 +43,9 @@ group :development do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+group :production do
+  gem 'pg'
+  gem 'rails_12factor'
 end
 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article,only: [:edit , :show ,:update, :destroy ]
 
   def index
     @article=Article.all
@@ -23,17 +24,12 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article =Article.find(params[:id]) 
-
   end
 
   def edit
-
-    @article=Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice]="Article was sucessfully Updated"
       redirect_to article_path(@article)
@@ -45,7 +41,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article=Article.find(params[:id])
     @article.destroy
     flash[:notice]="Article was Deleted"
     redirect_to articles_path
@@ -54,6 +49,11 @@ class ArticlesController < ApplicationController
   private
   def article_params
     params.require(:article).permit(:title,:description)
+  end
+
+  private 
+  def set_article
+    @article =Article.find(params[:id])
   end
 
 end
